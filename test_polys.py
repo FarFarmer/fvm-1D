c2f7cbef1755a87566cd3fc31ff1a0bece533d95
Grzegorz Karol Karch <karchgz@nin.visus.uni-stuttgart.de>
2016-09-22 23:29:41 +0200

    v

--- test_polys.py
+++ test_polys.py
@@ -3,29 +3,52 @@ import matplotlib.pyplot as plt
 import fvm
 
 polyOrder = 2
+spaceOrder = polyOrder+1
 res = 6
 f = np.linspace(0,0,res)
 f[2] = 1.0
 f[3] = 1.0
-
 u = np.linspace(1,1,res)
+dx = 2
+k = 3
 
-dx = 1
-dt = 0.125
+L = fvm.generateStencilMat(polyOrder)
+Linv = np.linalg.inv(L)
+S = fvm.generateOscillMat(polyOrder, dx)
 
-Linv = np.linalg.inv(generateStencilMat(polyOrder))
-S = generateOscillMat(polyOrder, dx)
+polyCoeffs = fvm.calcPolyCoeffs(polyOrder, f, k, Linv)
 
-polyCoeffs = np.ndarray(shape=(polyOrder+1,polyOrder+1))
-for j in range(polyOrder+1):
-    
-    # ids is used again to avoid if-else blocks regarding values on
-    # the boundary; it clamps the indices to [0:numCells-1]
-    ids = list(range(i-polyOrder+j,i+j+1))
-    for k in range(len(ids)):
-        ids[k] = max(0, min(ids[k], numCells-1))
-        
-        # scale = 2 because in the reference frame, dx = 2
-        scale = 2.0
-        phi = np.array([scale*f[x] for x in ids])
-        polyCoeffs[j] = Linv[j].dot(phi)
+#for each polynomial
+for i in range(-int(polyOrder/2),int(polyOrder/2)+1)
+    idx = 
+
+# omegas = fvm.calcOmega(polyCoeffs, S, polyOrder)
+# wenoCoeffs = np.transpose(polyCoeffs).dot(omegas)
+
+
+print(wenoCoeffs)
+
+
+x = np.linspace(-1,1,100)
+y = np.linspace(0,1,100)
+
+for i in range(len(x)):
+    y[i] = 0
+    for j in range(polyOrder):
+        y[i] += wenoCoeffs[j]*x[i]**j
+
+plt.plot(x, y)
+
+ind = np.arange(-0.5,res-0.5,1)
+width = 1
+plt.bar(ind, f, width, color='r')
+#plt.xlim(left=-0.5,right=res-0.5)
+plt.xlim(left=-1.5,right=res-0.5)
+plt.xlabel('x')
+plt.ylabel('f')
+plt.title('reconstruction')
+plt.grid(True)
+plt.show()
+
+
+
